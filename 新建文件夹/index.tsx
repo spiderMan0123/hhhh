8e454e3a2617f669588418cb62e21b186181d6da
wweac <2165974445@qq.com>
2025-06-04 10:52:38 +0800

    Add files via upload

---
+++ 新建文件夹/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css'; // 确保导入样式
+import App from './App'; // 确保导入App组件
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
